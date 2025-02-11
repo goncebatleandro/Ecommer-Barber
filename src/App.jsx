a1d5cf6058a55001ebf089e4e053dcd19f9abd56
Goncebat Leandro <leo.hasta.hoy@gmail.com>
2025-02-11 10:55:03 -0300

    Agregamos navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,47 @@ import './App.css'
 
 function App() {
 
-  return (
-    <h1>Bienvenidos a Aviles-barbershop</h1>
+// Realizaré un ecommerce para una barberia
+
+  return (  
+
+    <>
+    {/* Inicio del navbar */}
+      <header>
+        <nav className='nav-bar'>
+          <p>Logo barberia</p>
+          <ul className='links'>
+            <li className='links-item'>Inicio</li>
+            <li className='links-item'>Tienda</li>
+            <li className='links-item'>Contacto</li>
+          </ul>
+          <p>Ícono del carrito (3)</p>
+        </nav>
+      </header>
+      {/* Incio de las secciones */}
+      <section>
+        <div className='container-cards'>
+            <div className='card'>
+                  <h2>Producto 1</h2>
+                  <h3>Precio: 100$</h3>
+                  <button>Comprar</button>
+           </div>
+           <div className='card'>
+                  <h2>Producto 1</h2>
+                  <h3>Precio: 100$</h3>
+                  <button>Comprar</button>
+           </div>
+           <div className='card'>
+                  <h2>Producto 1</h2>
+                  <h3>Precio: 100$</h3>
+                  <button>Comprar</button>
+           </div>
+        </div>
+      </section>
+
+    </>
+
+
   );
 };
 
